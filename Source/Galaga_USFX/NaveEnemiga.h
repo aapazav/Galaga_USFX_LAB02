b1ad61505e6ec38544eefba3f566ab8df5e27b26
Ariel Apaza <arielapazav11@gmail.com>
2024-03-16 21:16:25 -0400

    Agregar archivos de proyecto.

---
+++ Source/Galaga_USFX/NaveEnemiga.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NaveEnemiga.generated.h"
+
+UCLASS()
+class GALAGA_USFX_API ANaveEnemiga : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Projectile, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* mallaNaveEnemiga;
+
+
+public:
+	/*UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Projectile, meta = (AllowPrivateAccess = "true"))
+	UStatic*/
+public:	
+	// Sets default values for this actor's properties
+	ANaveEnemiga();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+protected:
+	void Mover(float DeltaTime) PURE_VIRTUAL(ANaveEnemiga::Mover, );
+	//void Disparar(float DeltaTime) PURE_VIRTUAL(ANaveEnemiga::Disparar, );
+};
